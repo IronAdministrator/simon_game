143e47f2b418284b8bca4eb22ac36b4dfe43b728
IronAdministrator <74884776+IronAdministrator@users.noreply.github.com>
2021-09-15 18:40:12 +0200

    add onclick

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ let started = false;
 
 let level = 0;
 
-$(document).on("keypress", function () {
+$(document).on("keypress", "click", function () {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
