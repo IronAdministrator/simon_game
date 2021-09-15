2f4bce19855f4badd4d22679534682ffb4bbbf29
IronAdministrator <74884776+IronAdministrator@users.noreply.github.com>
2021-09-15 19:09:29 +0200

    chrome/safari compatibility

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ let started = false;
 
 let level = 0;
 
-$(document).on("touchstart keypress", function () {
+$(document).on("touchstart click keypress", function () {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
